68409264b88b13907e8879d1402255c4c95ea2f1
H-oxy.com <elphi008@gmail.com>
2020-07-04 11:20:37 -0400

    i'm doing great

---
+++ helllow.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 43
+	y := 26
+
+	fmt.Println(x + y)
+	fmt.Println("Hellow, playground", 26, false)
+
+}
--- main.go
+++
@@ -1,4 +0,0 @@
-package Forexample
-
-ff
-
